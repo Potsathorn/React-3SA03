1909016fcfbd8edb0c6be8b9beba8206fa915d88
Potsathorn <6010110237@psu.ac.th>
2019-09-22 02:09:15 +0700

    Game Logic v.1

--- src/WordCard.js
+++ src/WordCard.js
@@ -11,6 +11,7 @@ const prepareStateFromWord = (given_word) => {
  chars,
  attempt: 1,
  guess: [],
+ text: [],
  completed: false
  }
 }
@@ -21,33 +22,42 @@ export default class WordCard extends Component {
         super(props);
         this.state = prepareStateFromWord(this.props.value);
     }
-  
+
     activationHandler = (c) => {
+        
+        console.log(`${c} has been activated.`) 
         let guess = [this.state.guess] + c
+        let text = [this.state.guess] + c
         this.setState({guess})
+        this.setState({text})
+
         if(guess.length == this.state.chars.length){
-        if(guess == this.state.word){
-        this.setState({guess: [], completed: true})
-        }else{
-        let shuf = _.shuffle(Array.from(this.state.word))
-        this.setState({guess: [],chars : shuf, attempt: this.state.attempt + 1})
-        }
+            this.setState({text: [this.state.guess]})
+            if(guess == this.state.word){
+                this.setState({guess: [],text: [this.state.guess] + c, completed: true})
+                }
+            else{
+                let shuf = _.shuffle(Array.from(this.state.word))
+                this.setState({guess: [],text: [this.state.guess] + c,chars : shuf, attempt: this.state.attempt + 1})
+              }
         }
        }
 
 render() {
  return (
+   
  <div>
-     <p>ROUND : {this.state.attempt}</p>
+     <p>ROUND : {this.state.attempt}  </p>      
+     <p>YOUR WORD : {this.state.text}  </p>  
+      
 { Array.from(this.state.chars).map((c, i) => 
 <CharacterCard value={c} key={i} 
 activationHandler={this.activationHandler}
 attempt = {this.state.attempt}/>)}
-<p>{this.state.completed? "WIN!":""}</p>
-
-  
+<p>{this.state.completed? "WIN!":""}</p> 
 
  </div>
  );
+ 
  }
 }
