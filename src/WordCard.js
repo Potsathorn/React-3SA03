207ee5ddcafc799e4261c10b4cabf4dd3a32cd55
Potsathorn <6010110237@psu.ac.th>
2019-09-22 00:13:20 +0700

    Game Logic test3

--- src/WordCard.js
+++ src/WordCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
 import _ from 'lodash'; 
 
+
 const prepareStateFromWord = (given_word) => {
  let word = given_word.toUpperCase()
  let chars = _.shuffle(Array.from(word))
@@ -13,20 +14,23 @@ const prepareStateFromWord = (given_word) => {
  completed: false
  }
 }
+
 export default class WordCard extends Component {
+
     constructor(props){
         super(props);
-        this.setState = prepareStateFromWord(this.props.value);
+        this.state = prepareStateFromWord(this.props.value);
     }
   
     activationHandler = (c) => {
-        let guess = [...this.state.guess, c]
+        let guess = [this.state.guess] + c
         this.setState({guess})
-        if(guess.length === this.state.chars.length){
-        if(guess.join('').toString() === this.state.word){
+        if(guess.length == this.state.chars.length){
+        if(guess == this.state.word){
         this.setState({guess: [], completed: true})
         }else{
-        this.setState({guess: [], attempt: this.state.attempt + 1})
+        let shuf = _.shuffle(Array.from(this.state.word))
+        this.setState({guess: [],chars : shuf, attempt: this.state.attempt + 1})
         }
         }
        }
@@ -34,8 +38,15 @@ export default class WordCard extends Component {
 render() {
  return (
  <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>
-) }
+     <p>ROUND : {this.state.attempt}</p>
+{ Array.from(this.state.chars).map((c, i) => 
+<CharacterCard value={c} key={i} 
+activationHandler={this.activationHandler}
+attempt = {this.state.attempt}/>)}
+<p>{this.state.completed? "WIN!":""}</p>
+
+  
+
  </div>
  );
  }
