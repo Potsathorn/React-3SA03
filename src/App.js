2a89457265f366f11823ef9181900772b3936601
Potsathorn <6010110237@psu.ac.th>
2019-09-21 20:24:17 +0700

    make a Component Character

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import CharacterCard from './CharacterCard';
 class App extends Component {
  render() {
  return (
- <div className="App">
- Hello World
+ <div>
+ <CharacterCard value="h"/>
+<CharacterCard value="i"/>
  </div>
  );
  }
